61bb5a00a064cdf62a5299f7bc580d6c49d08361
SlothSan <mikeoram01@gmail.com>
2022-06-15 13:07:45 +0100

    06_leapYears complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+/* Leap Years are divisble by 4
+Leap Years divisible by 100 are not leap years
+Leap years that are divisible by 400 are leap years */
+
+
+// year remaider 4 = 0 leap year and year remainder 100 does not equal 0 or year remainder 400 does equal 0
+ return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 
 };
 
