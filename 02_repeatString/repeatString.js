372124fef2eda55d20622048a4fd77511023f77c
SlothSan <mikeoram01@gmail.com>
2022-06-15 11:20:47 +0100

    repeatString exercise complete - passes all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+/* Repeat a string x amount of times
+*/
 
+const repeatString = function(string, num) {
+    let repeatedString = ``;
+    if (num < 0) {
+        repeatedString = "ERROR"
+        return repeatedString;
+    }
+    while (num > 0) {
+        repeatedString += string;
+        num--
+    }
+  return repeatedString;
 };
 
 // Do not edit below this line
