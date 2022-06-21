8b5648cc5164080546662fe98fbbeb68db738d06
SlothSan <mikeoram01@gmail.com>
2022-06-21 19:27:31 +0100

    Fibonacci complete.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [0, 1];
+    if (num <= 0) {
+        return "OOPS"
+    }
 
+    if (typeof(num) == `string`) {
+        num = parseInt(num);
+    }
+    
+
+    for (i = 2; i <= num; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+
+    result = fib[num];
+    return result;
 };
 
 // Do not edit below this line
